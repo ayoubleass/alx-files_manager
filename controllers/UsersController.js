dcb991f0dc01cb09027844f6073ef0cd35ebb6a7
ayoubleass <ayoubleas@gmail.com>
2024-12-10 06:29:17 -0800

    Update  eslint requirement

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -5,25 +5,30 @@ import dbClient from '../utils/db';
 
 class UsersController {
   static async postNew(req, res) {
-    const { email, password } = req.body;
-    if (!email) {
-      return res.status(400).json({ error: 'Missing email' });
+    try {
+      const { email, password } = req.body;
+      if (!email) {
+        return res.status(400).json({ error: 'Missing email' });
+      }
+      if (!password) {
+        return res.status(400).json({ error: 'Missing password' });
+      }
+      const usersCollection = dbClient.db.collection('users');
+      const user = await usersCollection.find({ email });
+      if (user) {
+        return res.status(400).json({ error: 'Already exist' });
+      }
+      const hashPwd = sha1(password);
+      usersCollection.insertOne({ email, password: hashPwd });
+      const newUser = await usersCollection.findOne({ email });
+      return res.status(200).json({
+        id: newUser._id,
+        email: newUser.email,
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ error: 'Server error' });
     }
-    if (!password) {
-      return res.status(400).json({ error: 'Missing password' });
-    }
-    const usersCollection = dbClient.db.collection('users');
-    const user = await usersCollection.find({ email });
-    if (user) {
-      return res.status(400).json({ error: 'Already exist' });
-    }
-    const hashPwd = sha1(password);
-    usersCollection.insertOne({ email, password: hashPwd });
-    const newUser = await usersCollection.findOne({ email });
-    return res.status(200).json({
-      id: newUser._id,
-      email: newUser.email,
-    });
   }
 
   static async getMe(req, res) {
